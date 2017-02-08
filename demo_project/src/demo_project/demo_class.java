a027ee9cb606b14949416ee0f65c1b79200b27e6
rupeshwar <rupeshwar14@gmail.com>
2017-02-08 14:35:11 +0530

    Added Hello World Statement.

--- demo_project/src/demo_project/demo_class.java
+++ demo_project/src/demo_project/demo_class.java
@@ -3,7 +3,7 @@ package demo_project;
 public class demo_class {
 	public static void main(String args[])
 	{
-		System.out.println("Hello");
+		System.out.println("Hello World");
 /*		System.out.println("Hello");
 */	}
 
