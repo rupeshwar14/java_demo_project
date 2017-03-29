a9ab14c6c5f1b85162656632c3e6f9c1e67978e5
rupeshwar <rupeshwar14@gmail.com>
2017-03-29 14:43:16 +0530

    Create demo_class.java

---
+++ demo_class.java
@@ -0,0 +1,15 @@
+
+
+public class demo_class {
+	public static void main(String args[])
+	{
+		System.out.println("Hello World");
+/*		System.out.println("Hello");
+*/	
+		System.out.println("Bye");
+		System.out.println("Bye2");
+		System.out.println("Bye3");
+System.out.println("Bye4");
+		}
+
+}
